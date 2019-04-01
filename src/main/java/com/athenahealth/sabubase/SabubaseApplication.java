46efb7dc51e20b98aafb12a1ef67dac74b6c3ea1
Nidhi Kaushal <knidhi@ath017747.corp.athenahealth.com>
2019-04-01 14:42:29 +0530

    Adding base

---
+++ src/main/java/com/athenahealth/sabubase/SabubaseApplication.java
@@ -0,0 +1,13 @@
+package com.athenahealth.sabubase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabubaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabubaseApplication.class, args);
+	}
+
+}
