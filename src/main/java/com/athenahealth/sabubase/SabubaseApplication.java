99df02e258b20e26db6e56fedd50abb0752dd644
Nidhi Kaushal <knidhi@ath017747.corp.athenahealth.com>
2019-04-03 12:56:06 +0530

    avro

--- src/main/java/com/athenahealth/sabubase/SabubaseApplication.java
+++ src/main/java/com/athenahealth/sabubase/SabubaseApplication.java
@@ -1,13 +1,19 @@
 package com.athenahealth.sabubase;
 
+import com.athenahealth.Measure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SabubaseApplication {
 
+	static Measure measure;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SabubaseApplication.class, args);
+		measure = Measure.newBuilder().build();
+		measure.setTest("hi");
 	}
 
 }
